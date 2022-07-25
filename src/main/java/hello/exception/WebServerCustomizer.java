132d04eeb038b3628e2bd1afce6d3d68a72a8dc1
lby132 <quddbscjswow@naver.com>
2022-07-25 23:51:05 +0900

    단계별 Exception 처리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Component
+//@Component
 @Slf4j
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
